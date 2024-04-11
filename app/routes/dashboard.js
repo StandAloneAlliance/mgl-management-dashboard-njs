7fd7dbce707a52c522e3bb8e5811328fb2b9f3d0
antoniogisondi <anto.gisondi@tiscali.it>
2024-04-11 18:18:14 +0200

    feat/create routes for customers

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -6,4 +6,12 @@ router.get('/dashboard', isAuth(), (req, res) => {
     res.render('dashboard', { user: req.user })
 })
 
+router.get('/dashboard/customers', isAuth(), (req, res) => {
+    res.render('customers/index-customers')
+})
+
+router.get('/dashboard/customers/create-customers', isAuth(), (req, res) => {
+    res.render('customers/create-customers')
+})
+
 module.exports = router
