5d56957ca95bf2139d35d3eeff546c632e005049
antoniogisondi <anto.gisondi@tiscali.it>
2024-04-19 22:59:51 +0200

    fix

--- app/config/db-connection.js
+++ app/config/db-connection.js
@@ -1,10 +1,15 @@
 
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mssql',
+    dialectOptions: {
+        options: {
+            requestTimeout: 30000 // 30 secondi di timeout
+        }
+    }
 })
 
 module.exports = sequelize;
