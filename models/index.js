aa0f2349ba817c720297b16243bc836bdce9e9bc
antoniogisondi <anto.gisondi@tiscali.it>
2024-04-20 20:00:56 +0200

    fix another one

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
 'use strict';
-require('dotenv').config()
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.APP_ENV || 'productions';
-const configPath = path.resolve(__dirname, '../config/config.json');
+const env = process.env.NODE_ENV || 'production';
+const configPath = path.resolve(__dirname, './config/config.json');
 const config = require(configPath)[env];
 const db = {};
 
